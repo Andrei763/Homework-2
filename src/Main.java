b46d3cd836d5b5e55e79af53423d4b7f94b44ebb
Andrei <akifevandrej11@gmail.com>
2024-11-12 15:00:57 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int price = 12_412;
+        int bonus = price / 20;
+        System.out.println("Бонус:" + bonus);
+    }
+}
